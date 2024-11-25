3ad63bc51ed109878c76049ff27624689f1fe5aa
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-11-25 10:01:44 +0100

    refractor début

---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '/' => ['accueilController', 'Accueil'],
+    '/accueil' => ['accueilController', 'Accueil'],
+    '/mentionsLegales' => ['mentionsLegalesControleur', 'MentionsLegales'],
+    '/user-add' => ['userControleur', 'addUser'],
+    '/todos/toggle' => ['TodoController', 'toggle'],
+    '/todos/delete' => ['TodoController', 'delete'],
+    '/todos/show' => ['TodoController', 'show'],
+    '/legal' => ['HomeController', 'legal']
+];
--- public/index.php
+++ public/index.php
@@ -6,6 +6,42 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Récupération des routes
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+// Récupération de l'URL actuelle
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// Dans https://localhost:8000/todos/show?id=1
+// $uri = "/todos/show"
+
+// Recherche de la route correspondante
+if (!isset($routes[$uri])) {
+    $errorController = new \App\Controller\ErrorController();
+    $errorController->error404();
+    exit;
+}
+
+// Récupération du contrôleur et de l'action
+[$controllerName, $action] = $routes[$uri];
+$controllerClass = "App\\Controller\\{$controllerName}";
+
+// Récupération du contrôleur et de l'action
+[$controllerName, $action] = $routes[$uri];
+$controllerClass = "App\\Controller\\{$controllerName}";
+
+try {
+    // Instanciation du contrôleur et appel de l'action
+    $controller = new $controllerClass();
+    $controller->$action();
+} catch (\Exception $e) {
+    error_log($e->getMessage());
+    $errorController = new \App\Controller\ErrorController();
+    $errorController->error404();
+}
+
+
+
+
 /**
  * @var Doctrine\ORM\EntityManager $entityManager
  */
@@ -61,5 +97,51 @@ switch ($route){
 
 require __DIR__ . "/../views/_partial/footer.php";
 
+
+
+
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Récupération des routes
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+
+
+
+
+
+// Récupération de l'URL actuelle
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// Dans https://localhost:8000/todos/show?id=1
+// $uri = "/todos/show"
+
+
+
+
+// Recherche de la route correspondante
+if (!isset($routes[$uri])) {
+    $errorController = new \App\Controller\ErrorController();
+    $errorController->error404();
+    exit;
+}
+
+
+
+
+// Récupération du contrôleur et de l'action
+[$controllerName, $action] = $routes[$uri];
+$controllerClass = "App\\Controller\\{$controllerName}";
+
+try {
+    // Instanciation du contrôleur et appel de l'action
+    $controller = new $controllerClass();
+    $controller->$action();
+} catch (\Exception $e) {
+    error_log($e->getMessage());
+    $errorController = new \App\Controller\ErrorController();
+    $errorController->error404();
+}
 
 
