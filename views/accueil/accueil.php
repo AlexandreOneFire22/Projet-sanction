dcaa39a454133f5a9590978244f15fd0d3f4ff30
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-11-26 10:30:10 +0100

    fini2

--- views/accueil/accueil.php
+++ views/accueil/accueil.php
@@ -3,7 +3,7 @@
 
 <main class="container-fluid">
 
-<h1 class="text-primary fs-4"> Se site est un site web ayant pour vocation d'assiter les professeurs,
+<h1 class="text-primary fs-4" style="text-indent: 40px"> Se site est un site web ayant pour vocation d'assiter les professeurs,
     personnels scrolaire, et l'administration scrolaire sur les sanctions donné au élèves et étudiants.</h1>
 
 </main>
