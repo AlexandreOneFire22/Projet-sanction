051b15941f006c1c4a0109a1dcd70ed8f005655b
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-12-03 10:58:46 +0100

    promotion début

---
+++ src/Controllers/PromotionController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Entity\Promotion;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use App\Controllers\ErrorController;
+
+class PromotionController extends AbstractController
+{
+    private EntityManager $entityManager;
+
+    /**
+     * @var Doctrine\ORM\EntityManager $entityManager
+     */
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $this->entityManager->getRepository(Promotion::class);
+    }
+
+
+    public function creerUnCompte(){
+
+        if (isset($_SESSION ["user"])){
+            $pageErreur = new ErrorController();
+            $pageErreur->pageErreur("vous êtes déjà connecté à un compte.",
+                "Si vous souhaiter accèder à cette page vous devez d'abord vous déconnecter.",
+                "/seDeconnecter", "Se déconnecter");
+            exit();
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+
+            //Vérification des données saisie :
+
+            $prenom = $_POST["prenom"];
+            $nom = $_POST["nom"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $passwordVerif = $_POST["passwordVerif"];
+
+            $erreurs = [];
+
+            if (empty($prenom)) {
+                $erreurs ["prenom"] = "La saisie du prénom est obligatoire.";
+            }
+
+            if (empty($nom)) {
+                $erreurs ["nom"] = "La saisie du nom est obligatoire.";
+            }
+
+            if (empty($email)) {
+                $erreurs ["email"] = "L'adresse email est obligatoire.";
+            } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $erreurs ["email"] = "L'adresse email n'est pas valide.";
+
+            }elseif (!empty($this->repository->findOneBy(['email' => $email]))){
+                $erreurs ["email"] = "Cette adresse email est déjà utilisé.";
+            }
+
+            $passwordParLettre = str_split($password);
+
+            if (empty($password)){
+                $erreurs ["password"] [] = "Le mot de passe est obligatoire.";
+            }elseif (strlen($password)<8){
+                $erreurs ["password"] [] = "Le mot de passe doit comporter plus de 8 caractère.";
+            }
+
+            $tabPassword = str_split($password);
+
+            $minusculePresent = false;
+            $majusculePresent = false;
+            $chiffrePresent = false;
+
+            foreach ($tabPassword as $lettre) {
+
+                $assciiLetttre = ord($lettre);
+
+                switch (true) {
+                    case ($assciiLetttre >= 48 && $assciiLetttre <= 57):
+                        $chiffrePresent = true;
+                        break;
+
+                    case ($assciiLetttre >= 65 && $assciiLetttre <= 90):
+                        $majusculePresent = true;
+                        break;
+
+                    case ($assciiLetttre >= 97 && $assciiLetttre <= 122):
+                        $minusculePresent = true;
+                        break;
+                }
+            }
+
+            if (!$minusculePresent) {
+                $erreurs ["password"] [] = "Le mot de passe doit comporter au moins une minuscule.";
+            }
+
+            if (!$majusculePresent) {
+                $erreurs ["password"] [] = "Le mot de passe doit comporter au moins une majuscule.";
+            }
+
+            if (!$chiffrePresent) {
+                $erreurs ["password"] [] = "Le mot de passe doit comporter au moins un chiffre.";
+            }
+
+
+            if (empty($passwordVerif)) {
+                $erreurs ["passwordVerif"] = "Le mot de passe doit être à nouveau saisie.";
+            }elseif ($password!=$passwordVerif){
+                $erreurs ["passwordVerif"] = "Le mot de passe saisie est différent, il doit être identique.";
+            }
+
+            if (empty($erreurs)) {
+                //ajout des données dans la base de données :
+                $user = new User();
+                $user->setNom($_POST["nom"]);
+                $user->setPrenom($_POST["prenom"]);
+                $user->setEmail($_POST["email"]);
+
+                $passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+                $user->setPassword($passwordHash);
+
+                $this->entityManager->persist($user); //persist n'exécute pas directement le insert
+
+                //Valider le Insert
+
+                $this->entityManager->flush(); // flush Réalise le Insert
+
+                $this->render('accueil/accueil',"footerMoins");
+            }else{
+                $_SESSION ["erreurs"] = $erreurs;
+                $this->render('user/creerUnCompte',"footerPlus");
+            }
+        }else{
+            $this->render('user/creerUnCompte',"footerPlus");
+        }
+
+    }
+
+}
---
+++ src/Entity/Promotion.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "promotion")]
+class Promotion
+{
+    #[ORM\Id] // Clé primaire dans la table posts
+    #[ORM\Column(name: "id_promotion", type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int $id;
+
+    #[ORM\Column(name: "libelle_promotion", type: "string", length: 100)]
+    private string $libelle;
+
+    #[ORM\Column(name: "annee_promotion", type: "date")]
+    private string $annee;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLibelle(): string
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param string $libelle
+     */
+    public function setLibelle(string $libelle): void
+    {
+        $this->libelle = $libelle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAnnee(): string
+    {
+        return $this->annee;
+    }
+
+    /**
+     * @param string $annee
+     */
+    public function setAnnee(string $annee): void
+    {
+        $this->annee = $annee;
+    }
+
+}
